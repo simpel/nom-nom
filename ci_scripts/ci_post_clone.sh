7af638aeb59fb62e14e04d26571b7b6a62c6466d
Joel Sandén <me@joelsanden.se>
2026-09-04 12:48:13 +0200

    ci: add fallback plugin fingerprint validation key

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,6 @@
 # Disable Swift Macro fingerprint validation in Xcode Cloud
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-# Disable Swift Package Plugin fingerprint validation in Xcode Cloud (retains Apple's typo in key name)
+# Disable Swift Package Plugin fingerprint validation in Xcode Cloud
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
